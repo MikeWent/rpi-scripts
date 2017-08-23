958900c927e38fa9033eddce7ec6d8ef6a6aa035
Mike_Went <gitlab@meew.me>
2017-08-23 13:31:29 +0300

    Add Fahrenheit support

--- temperature.py
+++ temperature.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import os
 
+def celsius2fahrenheit(temp):
+    fahrenheit = 9.0/5.0 * temp + 32
+    return round(fahrenheit, 2)
+
 with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
     raw_cpu_temp = f.read()
 raw_gpu_temp = os.popen('/opt/vc/bin/vcgencmd measure_temp').read()
 
-cpu = round(int(raw_cpu_temp)/1000, 1)
-gpu = raw_gpu_temp[5:-3]
+cpu_c = round(int(raw_cpu_temp)/1000, 1)
+gpu_c = float(raw_gpu_temp[5:-3])
+cpu_f = celsius2fahrenheit(cpu_c)
+gpu_f = celsius2fahrenheit(gpu_c)
 
-print('CPU:', cpu)
-print('GPU:', gpu)
+print('CPU:', str(cpu_c)+'°C', str(cpu_f)+'°F')
+print('GPU:', str(gpu_c)+'°C', str(gpu_f)+'°F')
