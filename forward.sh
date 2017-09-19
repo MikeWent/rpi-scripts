d357a35cefae0400938e18711df0403ef1441c89
Mike_Went <gitlab@meew.me>
2017-09-19 08:32:17 +0000

    Add net.ipv4.ip_forward

--- forward.sh
+++ forward.sh
@@ -5,6 +5,7 @@
 # gitlab.com/Mike_Went/rpi-scripts
 ##
 
+sudo sysctl net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING --out-interface $2 -j MASQUERADE
 sudo iptables -A FORWARD --in-interface $1 -j ACCEPT
 
